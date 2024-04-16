8d2ceafef364ac1a75d4efaa1eadfa8babbb2293
gabriellml <bilynenov@gmail.com>
2024-04-16 11:42:05 -0300

    personagem creator

--- app/src/main/java/br/com/alura/personagemcreator/ui/activity/SelectCharactersViewModel.kt
+++ app/src/main/java/br/com/alura/personagemcreator/ui/activity/SelectCharactersViewModel.kt
@@ -5,14 +5,19 @@ import br.com.alura.personagemcreator.model.Personagem
 
 class SelectCharactersViewModel : ViewModel() {
 
-    private val _selectedCharacters = mutableListOf<Personagem>()
-    val selectedCharacters: List<Personagem> get() = _selectedCharacters
+    // ESTE VIEW MODEL AINDA NÃO ESTÁ SENDO UTILIZADO
 
-    fun toggleCharacterSelection(personagem: Personagem) {
-        if (_selectedCharacters.contains(personagem)) {
-            _selectedCharacters.remove(personagem)
-        } else {
-            _selectedCharacters.add(personagem)
-        }
-    }
+    // THIS VIEW MODEL IS NOT YET IN USE
+
+
+//    private val _selectedCharacters = mutableListOf<Personagem>()
+//    val selectedCharacters: List<Personagem> get() = _selectedCharacters
+//
+//    fun toggleCharacterSelection(personagem: Personagem) {
+//        if (_selectedCharacters.contains(personagem)) {
+//            _selectedCharacters.remove(personagem)
+//        } else {
+//            _selectedCharacters.add(personagem)
+//        }
+//    }
 }
